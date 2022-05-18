bdac5a524724430f39892aee0cb997233e96151e
Shaheem Khan <mshaheemkz@gmail.com>
2022-05-18 20:56:18 +0500

    fix crash error

--- src/helper/normalizeGenerateResults.ts
+++ src/helper/normalizeGenerateResults.ts
@@ -1,13 +1,17 @@
+import { HttpException, HttpStatus } from "@nestjs/common";
 import { GenerateKey } from "src/interfaces/generate.interface";
 
 export default (stdout: string): GenerateKey => {
     const regix = /Pubkey \(.*?\):\s+(\w+)\nPrivkey \(.*?\):\s+(\w+)\nAddress:\s+(\w+)\nPrivkey:\s+(\w+)/g;
     const match = regix.exec(stdout);
-    const result: GenerateKey = {
-      publicKeyHex: match[1],
-      privateKeyHex: match[2],
-      address: match[3],
-      privateKey: match[4]
+    if (match) {
+      const result: GenerateKey = {
+        publicKeyHex: match[1],
+        privateKeyHex: match[2],
+        address: match[3],
+        privateKey: match[4]
+      }
+      return result;
     }
-    return result;
+    throw new HttpException('Unable to generate key', HttpStatus.EXPECTATION_FAILED)
 }
